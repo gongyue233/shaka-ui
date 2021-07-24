e226a5d536ce6e394f7bb9d1498451e1298be0ec
baiyang <gongyue233@163.com>
2021-07-25 01:30:59 +0800

    初始化

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+
+import App from "./App.vue";
+import "./index.css";
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./components/Home.vue";
+
+
+const history = createWebHashHistory();
+const router = createRouter({
+  history: history,
+  routes: [{ path: "/", component: Home }],
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
