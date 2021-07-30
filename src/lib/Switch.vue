21845a7a633fe76401b24e7c3f174638441194d0
baiyang <gongyue233@163.com>
2021-07-30 18:27:13 +0800

    switch css2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,37 +1,52 @@
 <template>
-    <div>
-        <button class="shaka-switch-button">
-            <span class="shaka-switch-span"></span>
-        </button>
-    </div>
+  <div>
+    <button
+      class="shaka-switch-button"
+      :class="{ checked }"
+      @click="toggleSwitch"
+    >
+      <span class="shaka-switch-span"></span>
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
+
 export default {
-    name:'Switch'
+  name: "Switch",
+  setup() {
+    const checked = ref(false);
+    const toggleSwitch = () => {
+      checked.value = !checked.value;
+      console.log("checked的值" + checked.value);
+    };
+    return { checked, toggleSwitch };
+  },
 };
 </script>
 
 <style lang="scss">
-$switch-h:30px;
+$switch-h: 30px;
 $switch-color: #1687a7;
-.shaka-switch-button{
+.shaka-switch-button {
+  height: $switch-h;
+  width: $switch-h * 2;
+  border: none;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: $switch-h/2;
+  position: relative;
+  > .shaka-switch-span {
+    position: absolute;
+    border: 1px solid $switch-color;
+    right: -1px;
+    top: -1px;
     height: $switch-h;
-    width: $switch-h*2;
-    border: none;
-    background-color: $switch-color;
+    width: $switch-h;
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.2);
     border-radius: $switch-h/2;
-    position: relative;
-    > .shaka-switch-span{
-        position: absolute;
-        border: 1px solid $switch-color;
-        left: 0;
-        top: 0;
-        height: $switch-h;
-        width: $switch-h;
-        background-color: #f5f5f5;
-        border-radius: $switch-h/2;
-    }
+  }
 }
-
 </style>
