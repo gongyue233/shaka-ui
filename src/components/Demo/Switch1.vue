0b0356859cb5eda3a3a74f054355eb76cd9b0a07
baiyang <gongyue233@163.com>
2021-08-25 17:55:57 +0800

    展示源代码

---
+++ src/components/Demo/Switch1.vue
@@ -0,0 +1,19 @@
+<demo>
+  SwitchDemo文件
+</demo>
+<template>
+  <div>SwitchDemo</div>
+  <shaka-switch v-model:checked="checked" />
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import ShakaSwitch from "../../lib/ShakaSwitch.vue";
+export default {
+  components: { ShakaSwitch },
+  setup() {
+    const checked = ref(false);
+    return { checked };
+  },
+};
+</script>
---
+++ src/components/Doc/SwitchDemo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="switch-demo">
+    <h1>Switch 组件示例</h1>
+    <div class="demo">
+      <switch-1 />
+      <div class="code">
+        <pre>{{ Switch1.__sourceCode }}</pre>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Switch1 from "../Demo/Switch1.vue";
+export default {
+  name: "SwitchDemo",
+  components: { Switch1 },
+  setup() {
+    return {
+      Switch1, //必须 return 后才可以在 template 内展示
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.switch-demo {
+  border: 1px solid red;
+  padding: 20px;
+  > .demo {
+    border: 1px solid gray;
+    padding: 20px;
+    > .code {
+      border: 1px solid red;
+    }
+  }
+}
+</style>
