c656617d04705a52bb5a2598280c1ad360fbdd53
baiyang <gongyue233@163.com>
2021-09-01 19:46:16 +0800

    rollup.config.js

---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+/// <reference path="../shims-vue.d.ts"/>
+
+export { default as ShakaSwitch } from "./ShakaSwitch.vue";
+export { default as ShakaButton } from "./ShakaButton.vue";
+export { default as ShakaTab } from "./ShakaTab.vue";
+export { default as ShakaTabs } from "./ShakaTabs.vue";
+export { default as ShakaDialog } from "./ShakaDialog.vue";
+export { default as openShakaDialog } from "./openShakaDialog";
+    
